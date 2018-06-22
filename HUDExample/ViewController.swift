70b7c4894067c29e758d8b4d204d38f70b0bae68
rkuma199 <ranjith.c.kumar@capgemini.com>
2018-06-22 13:30:11 +0530

    Initial Commit

---
+++ HUDExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HUDExample
+//
+//  Created by Ranjith Kumar on 6/22/18.
+//  Copyright © 2018 DrawRect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
